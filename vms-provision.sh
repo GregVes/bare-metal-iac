24da18754099b7824a15d8a5cd9899945012e8dd
Gregory Vespasien <gregory.vespasien@curalie.com>
2022-04-27 23:36:15 +0200

    remove k8s install tasks from vms-provision.sh

--- vms-provision.sh
+++ vms-provision.sh
@@ -5,6 +5,3 @@ set -e
 ansible-playbook -t virt_disks_create -i inventory.yml playbook.yml --extra-vars "virt_root_password=helloworld" --limit=kvm_host
 
 ansible-playbook -t virt_vm_create -i inventory.yml playbook.yml --limit=kvm_host
-
-sleep 10
-ansible-playbook -t k8s_install -i inventory.yml playbook.yml --limit=kvm_guests
