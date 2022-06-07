98c1e7088f6cf17ca7792e36e6e409e9e41bee58
Gregory Vespasien <gregory.vespasien@curalie.com>
2022-06-07 20:05:21 +0200

    add additional disk to vm for k8s storage

--- roles/kubernetes/files/install-metallb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-
-kubectl get configmap kube-proxy -n kube-system -o yaml | \
-sed -e "s/strictARP: false/strictARP: true/" | \
-kubectl apply -f - -n kube-system
